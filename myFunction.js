c3beee32b3c051c0e03436a573b4dbe398da4c1c
karansamra <karansamra@gmail.com>
2016-09-28 21:15:54 +0530

    Enhancement in code to get all the values

--- myFunction.js
+++ myFunction.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
   $("form#user input[type=button]").click(function(){
-    //get the data of the textfield with id firstname.
+    //get value of the textfield with id firstname.
+    //get value of the textfield with id lastname.
     firstname = $('input#firstname').val();
+    lastname = $('input#lastname').val();
 
-    //get value of the text field with id lastname.
+    //get value of the textfield with id email.
+    emailid = $('input#email').val();
 
-    //get value of the checkboxes.
+    //get value of the textfield with id description.
+    descriptionofuser = $('input#description').val();
 
-    //get value of radio button
+    //get the value of the radio button with class gender.
+    gender = $("input[name=gender]:checked").val();
 
-    //etc...
+    //get the value of the checkboxes with different id's.
+    teacher = $('input#teacher').val();
+
+    //get the value of the checkboxes with different id's.
+    engineer= $('input#engineer').val();
+
+    //get the value of the checkboxes with different id's.
+    housewife= $('input#housewife').val();
+
+    //get the value of the checkboxes with different id's.
+    ca= $('input#ca').val();
+
 
     html = '<label>Firstname</label>: ';
-    html += firstname;
+    html += firstname+'<br>';
+    html += '<label>Lastname</label>: ';
+    html += lastname+'<br>';
+    html += '<label>Email ID</label>: ';
+    html += emailid+'<br>';
+    html += '<label>Description of user</label>: ';
+    html += descriptionofuser+'<br>';
+    html += '<label>Gender</label>: ';
+    html += gender+'<br>';
+    // html = '<label>Checkbox</label>: ';
+    // html += checkbox;
 
-
-    $('div#selectedData').html(html);
+    $('div#selectedData').html(html) ;
   })
 })
